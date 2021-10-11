29ab9ea793383db336abca1fe56ffe5a000ce67d
Oleksandr Balitskiy <popingalov@gmail.com>
2021-10-11 22:34:21 +0300

    firs reliz

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import './sass/main.scss';
 /* import './js/theme.js'; */
 import apiService from './js/apiService.js';
+import testHbs from './templates/gallery-homepage.hbs';
 /* const apiService = new ApiService(); */
-console.log(apiService.getTrend);
+console.log(apiService.getTrend());
+const gallery = document.querySelector('.gallery');
+console.log(gallery);
+
+async function testRender() {
+  const tryThis = await apiService.getTrend();
+  gallery.insertAdjacentHTML('beforeend', testHbs(tryThis.results));
+}
+testRender();
