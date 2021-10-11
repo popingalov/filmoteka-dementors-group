0ab00a69dfbfe175086115f4cdf6f1b0fc2e59b9
Oleksandr Balitskiy <popingalov@gmail.com>
2021-10-11 20:55:38 +0300

    js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import './sass/main.scss';
-//import './js/theme';
-//import './js/header';
+/* import './js/theme.js'; */
+import apiService from './js/apiService.js';
+/* const apiService = new ApiService(); */
+console.log(apiService.getTrend);
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,3 +1,64 @@
+import axios from 'axios';
+
+class FilmsApiProg {
+  constructor(key) {
+    this.filmURL = 'https://api.themoviedb.org/3';
+    this.key = key;
+  }
+
+  async getTrend() {
+    try {
+      const filmesFox = await axios.get(`${this.filmURL}/trending/movie/day?api_key=${this.key}`);
+      const trending = filmesFox.data;
+      return trending;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async getIdFilms(filmId) {
+    try {
+      const filmesFox = await axios.get(`${this.filmURL}/movie${filmId}?api_key=${this.key}`);
+      const films = filmesFox.data;
+      return films;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async getTreilsMovie(movie) {
+    try {
+      const filmesFox = await axios.get(
+        `${this.filmURL}/search/movie?api_key=${this.key}&movie=${movie}`,
+      );
+      const mabye = filmesFox.data;
+      return mabye;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async getFilmsTraing(traing) {
+    try {
+      const filmesFox = await axios.get(
+        `${this.filmURL}/movie/${traing}/videos?api_key=${this.key}&language=en-US`,
+      );
+      const traingData = filmesFox.data;
+      const traingFilm = `https://www.youtube.com/watch?v=${(traingData[0], key)}`;
+      return traingFilm;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async getBildFilm(film) {
+    try {
+      const filmesFox = await axios.get(`${this.filmURL}/movie/${film}?api_key=${this.key}`);
+      const progThink = filmesFox.data;
+      return progThink;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+export default new FilmsApiProg('7c9dd50606a07df965d51fc9621e1448');
+
 /* const API_KEY = '22470352-c65ac8cc67498f5a0ef8fcd03';
 const BASE_URL = 'https://pixabay.com/api';
 
