891796820d27982f5db070dcf8ec2e03fa228add
Dmitry Posokhin <79544402+NEGANN@users.noreply.github.com>
2021-10-12 14:07:31 +0300

    добавил refs и search

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './sass/main.scss';
 /* import './js/theme.js'; */
 import apiService from './js/apiService.js';
+import searchForm from './js/searchForm.js';
 /* const apiService = new ApiService(); */
 console.log(apiService.getTrend);
---
+++ src/js/refs.js
@@ -0,0 +1,18 @@
+/* 
+1.input
+2.галерея
+3.карточка
+4.перемикач на теми
+5.HOME
+6.MY LIBRARY
+
+*/
+export default {
+  searchForm: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  themeSwitch: document.querySelector('.theme-switch__control'),
+  homeBtn: document.querySelector('[data-button="home"]'),
+  libraryBtn: document.querySelector('[data-button="library"]'),
+  logo: document.querySelector('.js-logo'),
+  teamBtn: document.querySelector('.button-team'),
+};
