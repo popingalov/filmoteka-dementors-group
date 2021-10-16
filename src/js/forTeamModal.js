0c3bc63689926d101a7c11e61b94918df8418716
volokads <hluhovspg@gmail.com>
2021-10-16 19:21:39 +0300

    modal close by backdropClick

--- src/js/forTeamModal.js
+++ src/js/forTeamModal.js
@@ -1,14 +1,21 @@
 import refs from './refs.js'
 
 const {openTeamModalBtn, closeTeamModalBtn, modalTeam} = refs
-
+const back = document.querySelector('.backdrop')
+    console.log(back);
 const modalWindow = () => { 
   openTeamModalBtn.addEventListener('click', toggleModal);
   closeTeamModalBtn.addEventListener('click', toggleModal);
-  
+  back.addEventListener('click', (e) => {
+    if (e.target.classList == 'backdrop') {
+      modalTeam.classList.toggle('is-hidden')
+    }
+  })
+
   function toggleModal() {
   modalTeam.classList.toggle('is-hidden');
   }
+
 };
 modalWindow()
 
